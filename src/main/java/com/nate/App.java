c2a9a4a8f1a295ea3da51993b1d78d6f0b9402fa
nkramber <natekramber@gmail.com>
2025-01-22 03:02:46 -0600

    Initial commit

---
+++ src/main/java/com/nate/App.java
@@ -0,0 +1,15 @@
+package com.nate;
+
+import java.awt.Canvas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
